a2bd0de3c86fa23a8ecdadf9ce00a2a5324fd687
patrickmgebhard <patrickmgebhard@gmail.com>
2022-11-25 20:10:09 +0100

    testing committing and pushing a local repo back to github

--- network.py
+++ network.py
@@ -1,3 +1,5 @@
+# testing how to commit and push stuff back to GitHub
+
 import random
 import numpy as np
 
@@ -9,4 +11,4 @@ class Network(object):
     self.biases = [np.random.randn(y, 1) for y in sizes[:1]]
     self.weights = [np.random.rand(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-print("test")
+print("this is the next text ")
