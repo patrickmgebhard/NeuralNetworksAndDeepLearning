add9ad94c7e769c9b5d4ba9ec9c91472b0945fea
patrickmgebhard <patrickmgebhard@gmail.com>
2022-11-26 14:55:21 +0100

    added mnist_loader and added code for backprop, still  need to understand numpy arrays in detail

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,1 +1,40 @@
 # load the MINIST dataset here
+import cPickle 
+import gzip
+
+import numpy as np
+
+# input are the images
+def load_data():
+    f = gzip.open('./data/mnist.pkl.gz', 'rb')
+    # how does the cPickle know how to separate the data
+    training_data, validation_data, test_data = cPickle.load(f)
+    f.close()
+    #return (training_data[0].shape, training_data[0][0], training_data[1].shape, training_data[1][0], validation_data[0].shape, validation_data[1].shape, test_data[0].shape, test_data[1].shape,)
+    return(training_data, validation_data, test_data)
+
+def load_data_wrapper_pg():
+    tr_d, va_d, te_d = load_data()
+    # training data should be a list containing 50,000 2-tuples (x, y) where x is a 784-dimensional numpy.ndarray with the input image and y is a 10-dimensional numpy.ndarray representing the unit vector corresponding to the correct digit for x
+    #print(tr_d, len(tr_d), tr_d[0].shape, tr_d[1].shape)
+    # this is already in the correct form
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]] 
+    # bring y in correct form
+    training_results = [vectorized_result(y) for y in tr_d[1]] 
+    # bring x & y together using zip()
+    training_data = zip(training_inputs, training_results)
+    # validation and test data are lists containing 10,000 2-tuples (x, y) where x is a 784-dimensional array and y is the integer value of the corresponding x
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = zip(validation_inputs, va_d[1])
+    test_inputs = [np.reshape(x, (784,1 )) for x in te_d[0]]
+    test_data = zip(test_inputs, te_d[1])
+    return (training_data, validation_data, test_data)
+
+def vectorized_result(j):
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
+print(load_data_wrapper_pg())
+
+# output are the images split into training, test and validation dataset    
--- network.py
+++ network.py
@@ -23,9 +23,64 @@ class Network(object):
       print(a)
     return a
 
-  #reminder the test_data=None is a default value, if no variable is passed
+  # reminder the test_data=None is a default value, if no variable is passed
+  # the epoches is the number of times the algorithm goes through all the training data
+  # for each epoch
+    # we create mini batches from the training data and interate through all mini batches updating the weights and biases after each mini batch
+    # if test_data is provided the algorithm is evaluated after each epoch
   def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-    print("yo momma")
+    if test_data: n_test = len(test_data)
+    n = len(training_data)
+    for j in xrange(epochs:
+      random.shuffle(training_data)
+      # xrange(start, stop, step size)
+      mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+      for mini_batch in mini_batches:
+        self.update_mini_batch(mini_batch, eta)
+      if test_data:
+        print "Epoch {0}: {1} / {2}".format(j, self.evalue(test_data), n_test)
+      else:
+        print "Epoch {0} complete".format(j)
+
+  def update_mini_batch(self, mini_batch, eta):
+    # TODO so basically the cost function derivatives in the mini batch are added up and then the average is subtracted/added from the weight/bias
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    for x, y in mini_batch:
+        delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+        nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+        nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+    self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+    self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+  
+
+
+  # give a training sample and return a the gradient for the cost function
+  def backprop(self, x, y):
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    # feedforward why is self.feedforward not use
+    activation = x
+    activations = [x]
+    zs = []
+    for b, w in zip(self.biases, self.weights):
+      z = np.dot(w, activation) + b
+      zs.append(z)
+      activation = sigmoid(z)
+      activations.append(activation)
+    # backward pass
+    # not so sure if that syntax will work
+    delta = self.cost_derivative(activations[-1, y]) * \ sigmoid_prime(zs[-1]))
+    nabla_b[-1] = delta
+    nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+    for l in xrange(2, self.num_layers):
+      z = zs[-l]
+      sp = sigmoid_prime(z)
+      delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+      nabla_b[-l] = delta
+      nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+    return (nabla_b, nabla_w)
+ 
 
   # this function returns the no. of correctly guessed examples
   def evaluate(self, test_data):
